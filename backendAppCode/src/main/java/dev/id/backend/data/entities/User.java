7364b4522ac799aa10d207688d8dc3d5cfc131a1
jDevID <dav.botton@gmail.com>
2023-03-26 22:47:16 +0200

    impl security user entity

--- backendAppCode/src/main/java/dev/id/backend/data/entities/User.java
+++ backendAppCode/src/main/java/dev/id/backend/data/entities/User.java
@@ -1,35 +1,84 @@
 package dev.id.backend.data.entities;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import dev.id.backend.logic.security.models.entity.Role;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @ToString
-@Table(name = "users")
-@SuppressWarnings("unused") // TODO: SECURITY ALONG ANGULAR
-public class User extends BaseEntity {
-    @Column(name = "username")
-    private String username;
+@RequiredArgsConstructor
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@Table(name = "_user") //postgres already allocates a User Table
+public class User extends BaseEntity implements UserDetails {
+    private String lastname;
+    private String firstname;
     @Email
-    @Column(name = "mail")
     private String email;
-    @Column(name = "password")
     private String password;
-    @Column(name = "role")
-    private String role;
-
+    @Enumerated(EnumType.STRING) //ordinal by default
+    private Role role;
     @OneToMany(mappedBy = "user")
     @ToString.Exclude
     private List<Project> projects;
+
+    @Override // returns a list of roles
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name())); //take one
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; //
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { // false (expired) by default
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() { // f
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() { // f
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() { // f
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return getId() != null && Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
