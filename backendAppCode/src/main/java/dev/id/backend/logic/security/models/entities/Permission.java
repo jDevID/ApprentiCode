8f094f6a505b2d6723710b5713b794b623ae78a9
jDevID <dav.botton@gmail.com>
2023-03-27 12:08:42 +0200

    minor fix Permission attribut description

--- backendAppCode/src/main/java/dev/id/backend/logic/security/models/entities/Permission.java
+++ backendAppCode/src/main/java/dev/id/backend/logic/security/models/entities/Permission.java
@@ -1,15 +1,17 @@
-package dev.id.backend.logic.security.models.entity;
+package dev.id.backend.logic.security.models.entities;
 
+import dev.id.backend.logic.dtos.BaseDto;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity @Data
-public class Role {
+@Entity
+@Data
+public class Permission extends BaseDto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(unique = true)
     private String name;
+    private String description;
 
 }
