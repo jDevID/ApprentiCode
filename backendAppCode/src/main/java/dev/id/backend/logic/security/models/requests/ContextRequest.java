d575dc9b8d041279783a6bda18e6659d648f536f
jDevID <dav.botton@gmail.com>
2023-03-27 12:09:09 +0200

    created ContextRequest

---
+++ backendAppCode/src/main/java/dev/id/backend/logic/security/models/requests/ContextRequest.java
@@ -0,0 +1,13 @@
+package dev.id.backend.logic.security.models.requests;
+
+public class ContextRequest {
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
