08d7f793e0cba6fad5e3a25e9e5351c360c4dc80
jDevID <dav.botton@gmail.com>
2023-03-30 12:21:33 +0200

    created search-bar component

--- frontendAppCode/src/app/components/search-bar/search-bar.component.ts
+++ frontendAppCode/src/app/components/search-bar/search-bar.component.ts
@@ -1,16 +1,34 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-
+import {Component, EventEmitter, OnInit, Output, HostListener} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit {
+  searchControl = new FormControl();
+
   @Output() search = new EventEmitter<string>();
   searchQuery = '';
-
   onSearch(): void {
     this.search.emit(this.searchQuery);
+  }
+
+  constructor() {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((query) => {
+        // Call your search API with the query
+      });
+  }
+
+  @HostListener('window:keydown.enter', ['$event'])
+  onEnterKeyDown(event: KeyboardEvent): void {
+    event.preventDefault();
+    // Call your search function here
+  }
+  ngOnInit(): void {
   }
 }
